072987023b51ac002587e8780a3df9d044a5075b
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-12-30 09:28:23 +0530

    Adding terraform modue of k8

---
+++ mini-kube.tf
@@ -0,0 +1,22 @@
+module "minikube" { 
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "roboshop"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/.ssh/minikube.pub"
+  aws_subnet_id = "subnet-0f624afaf2edc76e1" # Replace your default subnet id
+  hosted_zone = "nanda.cfd"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
